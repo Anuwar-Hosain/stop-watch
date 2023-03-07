69266ed9029920affeeac43bcf1fe7aaa3f69bb6
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-03-07 15:01:39 +0600

    start btn ok

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const display = document.getElementById("display");
-var id;
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+let id;
 let sum = 0;
 document.getElementById("start").addEventListener("click", function () {
   id = setInterval(() => {
     let num = sum++;
     display.innerHTML = num;
   }, 1000);
+  startBtn.setAttribute("disabled", true);
+  startBtn.classList += " block";
 });
 function stopDisplay() {
   clearInterval(id);
+  startBtn.removeAttribute("disabled");
+  startBtn.classList.remove("block");
 }
 function reset() {
   sum = 0;
